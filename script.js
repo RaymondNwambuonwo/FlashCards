98e4e4852a327e5564992e282b8431c3c6ac2e9e
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-10-25 10:28:32 -0400

    added flash card information

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ let people = [
     ["Nas", "This artist hails from Queens, NY and played a crutial part in the resurgence of New York/Eastcoast rap during the early 90's. With his first album painting a vivid picture of what it was like to live in New York City, especially in housing projects. He has gone on to make history, having released multiple bodies of work, multiple investments, and even settling a feud with another fellow NYC Hip-Hop legend. Who is this? ", "Images/Nas.jpg"],
     ["Biggie", "To many people he was one of, if not the best rapper to ever pick up a microphone and single handedly brought New York back to the front lines of Hip-Hop with his debut album Ready To Die. With legendary hits like Juicy, One More Chance, and Hypnotize, he is undeniably one of the greatest to ever do it. Who is this?", "Images/Biggie.jpg"],
     ["Drake", "Often considered to be the greatest of the new generation, he made it cool for an artist to rap and sing. This artist has consistently been on the billboard for 10 years now, making hit after hit. Considered to have also changed the approach and idea of what a traditional mixtape was, with his breakout mixtape So Far Gone. He is also an actor, which he did first before music. Ladies love him and guys want to be him. Who is this? ", "Images/drake.jpg"],
-    ["Eminem", "Eminem", "Images/eminem.jpg"],
-    ["Jay Z", "Jay Z", "Images/jayz.jpg"],
-    ["lil Wayne", "Lil Wayne", "Images/lilwayne.jpg"],
-    ["Big L", "Big L", "Images/bigL.jpg"]
+    ["Eminem", "In a predominately black art form and genre, this artist has managed to not only dominate but also solidify himself in Hip-Hop. Never to far from the conversation of who the greats are, his contraversial, witty, and often out of this world lyrics have catapulted him into superstar status. Who is this? ", "Images/eminem.jpg"],
+    ["Jay Z", "From a hustler growing up in one of Brooklyn, New York's toughest areas to now being probably hip-hop's biggest mogul. He has stood the test of time in his 25 plus year career, being able to consistently reinvent himself as well as becoming hip-hop's first billionaire. He is also married to one of the Queens of R&B. Who is this?", "Images/jayz.jpg"],
+    ["lil Wayne", "Coming straight out of New Orleans and having been in the spotlight since the age of 14, this artist nothing short of a house hold name. With hits such as Lolipop, Mrs. Officer, and Go DJ to name a few, he has become a staple in Hip-Hop and many artist of today credit him as their main influence. Who is this?", "Images/lilwayne.jpg"],
+    ["Big L", "This artist hails from New York, New York or more specifically Harlem. He is considered to be one of the greatest rappers to ever touch a mic due to his extremely witty and outlandish lyrics. Having been one of the first succesful artist to come out of Harlem, he also had a hand in the resurgence of punchline and lyrical rap. Who is this?", "Images/bigL.jpg"]
 ]
 
 // Need and input box
